3e6a3cd9a0c7cb43f956810684ab61a4495b69e9
LefyDrums <LefyDrums@gmail.com>
2024-05-22 18:59:53 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,227 @@
-"""
-Run the sldviewer program.
-"""
-
-import sldviewer
+<?xml version="1.0" encoding="UTF-8"?>
+<StyledLayerDescriptor version="1.0.0" xsi:schemaLocation="http://www.opengis.net/sld StyledLayerDescriptor.xsd" xmlns="http://www.opengis.net/sld" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:gml="http://www.opengis.net/gml">
+  <NamedLayer>
+    <Name>Delicutual</Name>
+    <UserStyle>
+      <Title>Delicutual Symbolization</Title>
+      <FeatureTypeStyle>
+        <Rule>
+          <Name>Robo</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/Qcejw3M.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>18</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Hurto</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/itAWyYO.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>18</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Asalto y/o Robo c/Arma</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/uZwrlcZ.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>18</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Homicidio</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/n0OBSNv.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>18</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Hurto Automotor</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/htwTynD.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>18</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Incendio Automotor</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/lIvvJQS.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>17</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Robo Automotor</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/0SYVKdG.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>18</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Robo c/Arma Blanca</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/9arVcC5.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>18</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Suicidio</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/sna8mQ0.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>17</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Ttva. Hurto Automotor</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/A4SknW1.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>18</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Ttva. Robo Automotor</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/GhQJETg.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>18</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Ttva. Robo C/Arma</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/Pz7jsH6.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>18</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Ttva. Robo C/Arma Blanca</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/dyZLi9G.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>18</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Ttva. de Homicidio</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/YFH7Rh6.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>18</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Ttva. de Hurto</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/RtspD95.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>18</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Ttva. de Robo</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/XCYdcZL.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>18</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Femicidio</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/s6QveEc.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>16</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Ttva. de Femicidio</Name>
+          <PointSymbolizer>
+            <Graphic>
+              <ExternalGraphic>
+                <OnlineResource xlink:href="https://i.imgur.com/DCXxDhp.png"/>
+                <Format>image/png</Format>
+              </ExternalGraphic>
+              <Size>16</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+      </FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor>
